7c00975c78aa89c2e70f9b1acd17494abf576872
Tan Ahmed <tannerahmed@gmail.com>
2025-10-16 16:40:40 +0100

    setup react with shadcn & tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
